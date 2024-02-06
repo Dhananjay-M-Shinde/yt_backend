c688f244a06d8c524cf27b0db231c3cc21930a8d
Dhananjay shinde <dhanajayshinde2212@gmail.com>
2024-02-06 23:44:59 +0530

    app.js and utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+const asyncHandler = (reqHandler) => {
+    (req, res, next) =>{
+        Promise.resolve(reqHandler(req, res, next)).catch((err) =>{
+            next(err)
+        })
+    }
+}
+
+
+
+
+
+
+
+export {asyncHandler}
+
+// below is a higher order function i.e function which accept another function as an argument
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
