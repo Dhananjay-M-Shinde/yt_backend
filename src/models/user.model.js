edf643e9cb9c4682b24309a80fe5117f081f3b06
Dhananjay shinde <dhanajayshinde2212@gmail.com>
2024-02-24 02:33:34 +0530

    minor changes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -67,4 +67,30 @@ userSchema.methods.isPasswordCorrect = async function (password) {
    return await bcrypt.compare(password, this.password)
 }
 
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            userName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 export const User = mongoose.model('User', userSchema)
