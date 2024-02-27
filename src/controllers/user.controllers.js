772b1d915361047f2305f30c1c8a537561cc10b6
Dhananjay shinde <dhanajayshinde2212@gmail.com>
2024-02-27 23:58:22 +0530

    refresh token endpoint bug fix

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -209,14 +209,14 @@ const refreshAccessToken = asyncHandler(async (req, res) =>{
             secure: true
         }
     
-        const {accessToken, newRefreshToken} = await generateAccessAndRefreshToken(user._id)
+        const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id)
         return res
         .status(200)
         .cookie("accessToken", accessToken, options)
-        .cookie("refreshToken", newRefreshToken, options)
+        .cookie("refreshToken", refreshToken, options)
         .json(
             200,
-            {accessToken, refreshToken: newRefreshToken},
+            {accessToken, refreshToken},
             "Access token refreshed"
         )
     } catch (error) {
